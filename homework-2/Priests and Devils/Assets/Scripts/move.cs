987cc6da0be51a456b2138137454e21231cee8a6
Alicizations <673386754@qq.com>
2018-04-07 22:55:13 +0800

    modified

---
+++ homework-2/Priests and Devils/Assets/Scripts/move.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Mygame
+{
+
+
+    public class move : MonoBehaviour
+    {
+        public float speed = 5.0f;
+
+        public void Strat()
+        {
+            
+        }
+
+        // move
+        public void Update()
+        {
+            if (Director.GetInstance().CurrentScenceController.GetMoving())
+            {
+                if (Director.GetInstance().CurrentScenceController.GetFrom())
+                {
+                    this.transform.position = Vector3.MoveTowards(this.transform.position, new Vector3(-5, 1, 0), speed * Time.deltaTime);
+                    if (this.transform.position == new Vector3(-5, 1, 0))
+                    {
+                        Director.GetInstance().CurrentScenceController.ChangeFrom();
+                        Director.GetInstance().CurrentScenceController.StopMoving();
+                        // check win
+                    }
+                }
+                else
+                {
+                    this.transform.position = Vector3.MoveTowards(this.transform.position, new Vector3(5, 1, 0), speed * Time.deltaTime);
+                    if (this.transform.position == new Vector3(5, 1, 0))
+                    {
+                        Director.GetInstance().CurrentScenceController.ChangeFrom();
+                        Director.GetInstance().CurrentScenceController.StopMoving();
+                        // check win
+                    }
+                }
+            }
+        }
+    }
+}
