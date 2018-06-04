f2f754a40898afe45dfe16e9ecdf76b6a5cfff93
Alicizations <673386754@qq.com>
2018-06-04 23:05:02 +0800

    hw8

---
+++ homework-8/README.md
@@ -0,0 +1,7 @@
+## Particle
+
+用Unity3d做出来的DNF背包系统
+
+[参考博客](https://blog.kinpzz.com/2016/05/21/unity3d-ugui-Inventory/)
+
+[视频地址](https://www.bilibili.com/video/av24388378/)
---
+++ homework-8/UI/Assets/OnButtonClick.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OnButtonClick : MonoBehaviour {
+
+    public Sprite source;
+    public Sprite aim;
+
+    public Transform mtrans;
+
+    public bool init = true;
+
+    public GameObject relative;
+
+    public GameObject p;
+    public ParticleSystem ss;
+
+     void Awake()
+    {
+        mtrans = this.transform;
+        if (p != null)
+        {
+            ss = p.GetComponent<ParticleSystem>();
+        }
+    }
+
+
+    public void On_Click()
+    {
+        if (init)
+        {
+            mtrans.GetComponent<Image>().sprite = aim;
+            init = false;
+            relative.GetComponent<OnButtonClick>().initial();
+        }
+    }
+
+    public void initial()
+    {
+        mtrans.GetComponent<Image>().sprite = source;
+        init = true;
+        if (ss != null)
+        {
+            ss.Play();
+        }
+    }
+
+    // Use this for initialization
+     void Start () {
+        if (ss != null)
+        {
+            ss.Stop();
+        }
+    }
+	
+	// Update is called once per frame
+	 void Update () {
+		
+	}
+}
